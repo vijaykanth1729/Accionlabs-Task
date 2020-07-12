f933ee5d40fd0f0dec02d5dd2f0430b40b541529
vijaykanth1729 <personalvijay007@gmail.com>
2020-07-12 18:13:56 +0530

    pushing static files

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -127,4 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = (os.path.join(os.path.dirname(__file__),'..','static'))
 LOGIN_REDIRECT_URL = 'blog:home'

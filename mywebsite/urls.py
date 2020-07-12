2addb41b3cf0a4ccbb94fdc056db88359aaa69f1
vijaykanth1729 <personalvijay007@gmail.com>
2020-07-12 16:07:39 +0530

    Updated with swagger

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+schema_view = get_swagger_view(title='Blog Post Api')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api_documentation/', schema_view),
     path('', include('blog.urls', namespace='blog')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
